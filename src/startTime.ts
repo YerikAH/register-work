3f2da7935523a8e032f5b783a92d2a3c62aaa20e
Harvey Yerik <yerik05vh@gmail.com>
2022-12-08 17:09:50 -0500

    start notification dev

--- src/startTime.ts
+++ src/startTime.ts
@@ -1,16 +1,22 @@
 import { hourToSecond } from "./helpers/hourToSecond.js";
 import { DataTime } from "./interfaces/interface.js";
 import { WorkSave } from "./interfaces/variables.js";
+import timer from "./timer.js";
 
 export default function startTime() {
+  const $timeTemp = document.getElementById("timeTemp")!;
   setInterval(timeIsEqual, 1000);
-
+  let activateTimer = false;
+  let activateTimerHour: number;
+  let timerOut: number;
   function timeIsEqual() {
     const timeFather = new Date();
     const getHourCurrent: number = timeFather.getHours();
     const getMinuteCurrent: number = timeFather.getMinutes();
     const getSecondCurrent: number = timeFather.getSeconds();
     const getTimeAlarm = localStorage.getItem(WorkSave);
+
+    console.log(getHourCurrent, getMinuteCurrent, getSecondCurrent);
     if (typeof getTimeAlarm === "string") {
       const stringToArray: DataTime[] = JSON.parse(getTimeAlarm);
       stringToArray.forEach((item) => {
@@ -24,10 +30,27 @@ export default function startTime() {
           getSecondCurrent === getSecond
         ) {
           console.log("Start Timer");
+          clearTimeout(timerOut);
+          activateTimerHour = getTimeHour;
+          activateTimer = true;
+          timer();
         }
       });
     } else {
       localStorage.setItem(WorkSave, "[]");
+    }
+    if (activateTimer) {
+      timerOut = setTimeout(timerOutFunction, 1000);
+      function timerOutFunction() {
+        if (activateTimerHour !== 0) {
+          activateTimerHour--;
+          $timeTemp.textContent = `${activateTimerHour}`;
+        } else {
+          activateTimer = false;
+          $timeTemp.textContent = `3600`;
+          clearTimeout(timerOut);
+        }
+      }
     }
   }
 }
